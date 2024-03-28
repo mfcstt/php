da38cb5a891810bbb156e28a8162051672f474a2
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2024-03-28 15:28:08 +0000

    Refactor delete.php to handle image deletion

--- UploadArquivo/delete.php
+++ UploadArquivo/delete.php
@@ -1,7 +1,14 @@
 <?php 
 
-$imagens = filter_input_array("imagem");
-echo var_dump ($imagens);
-die();
+$imagens = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+// 
+if(!empty($imagens)){
+    foreach ($imagens as $linha) {
+        foreach ($linha as $key => $value) {
+        echo $key , "->" , $value ,"<br>";
+        unlink($value);
+    }
+}
+}
 
 ?>
